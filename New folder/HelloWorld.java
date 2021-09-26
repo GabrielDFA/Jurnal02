5053492d21bab77d0c114bffcb41922cc97d41ac
GabrielDFA <gdfayal@gmail.com>
2021-09-26 23:10:17 +0700

    HelloWorld.java

---
+++ New folder/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama: Gabriel David Florens Ayal!");
+		System.out.println("NIM: 6706213027");
+		System.out.println("Kelas: 45-03");
+	}
+}
+		
